b270116bca94d0b1b1056493b5908acf9e5be68c
GoodGuyTn <tuanphan1712005@gmail.com>
2024-10-26 12:58:07 +0700

    Thêm một số hàm vào ctool, vào discord để check là hàm gì nha

--- ctool.cpp
+++ ctool.cpp
@@ -1,4 +1,4 @@
-#include "ctool.h"
+﻿#include "ctool.h"
 mutex ctool::mtx;
 void ctool::ShowConsoleCursor(bool showFlag) {
 	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -17,4 +17,44 @@ void ctool::Draw(char* str, int i, cpoint p[], cpoint& _p) {
 	ctool::GotoXY(_p.getX(), _p.getY());
 	cout << str;
 	mtx.unlock();
-}
+}
+
+void ctool::setLocale() {
+	// Đặt locale để hỗ trợ ký tự Unicode
+	locale::global(locale("en_US.UTF-8"));
+	wcout.imbue(locale());
+}
+void ctool::resizeConsole(int width, int height)
+{
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, width, height, TRUE);
+}
+void ctool::setColor(ConsoleColor backgroundColor, ConsoleColor textColor) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// Thiết lập thuộc tính màu với nền và chữ
+	SetConsoleTextAttribute(hConsole, backgroundColor * 16 + textColor);
+}
+
+void ctool::setConsoleBackgroundColor(ConsoleColor backgroundColor) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// Lấy thông tin kích thước của console
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hConsole, &csbi);
+
+	// Đặt màu nền cho tất cả các ký tự trong console
+	DWORD consoleSize = csbi.dwSize.X * csbi.dwSize.Y;
+	DWORD charsWritten;
+
+	// Mã màu nền mới
+	WORD colorAttribute = backgroundColor << 4; // Dịch màu nền vào 4 bit cao
+
+	// Điền toàn bộ màn hình với màu nền mới
+	FillConsoleOutputAttribute(hConsole, colorAttribute, consoleSize, { 0, 0 }, &charsWritten);
+
+	// Di chuyển con trỏ về đầu màn hình
+	SetConsoleCursorPosition(hConsole, { 0, 0 });
+}
--- ctool.h
+++ ctool.h
@@ -1,13 +1,44 @@
 #pragma once
+
+#ifndef _CTOOL_H_
+#define _CTOOL_H_
+
 #include "cpoint.h"
 #include <mutex>
 #include <windows.h>
 #include <iostream>
+
 using namespace std;
+
+enum ConsoleColor {
+    Black = 0,
+    DarkBlue = 1,
+    DarkGreen = 2,
+    DarkCyan = 3,
+    DarkRed = 4,
+    DarkMagenta = 5,
+    DarkYellow = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    Blue = 9,
+    Green = 10,
+    Cyan = 11,
+    Red = 12,
+    Magenta = 13,
+    Yellow = 14,
+    White = 15
+};
+
 class ctool {
 public:
-	static mutex mtx;
-	static void ShowConsoleCursor(bool);
-	static void GotoXY(int x, int y);
-	static void Draw(char*, int, cpoint[], cpoint&);
-}
+    static mutex mtx;
+    static void ShowConsoleCursor(bool);
+    static void GotoXY(int x, int y);
+    static void Draw(char*, int, cpoint[], cpoint&);
+    static void setLocale();
+    static void resizeConsole(int, int);
+    static void setColor(ConsoleColor, ConsoleColor);
+    static void setConsoleBackgroundColor(ConsoleColor);
+};
+
+#endif
