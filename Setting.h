295d27b43b2884cd98bc4347e7ade42816a6e3f5
Huynh M. Doan <166630760+Geedie@users.noreply.github.com>
2024-11-07 12:53:46 +0700

    upload setting.h

---
+++ Setting.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef SETTING_H
+#define SETTING_H
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+class Setting {
+public:
+	void SettingControl();
+};
+
+#endif
