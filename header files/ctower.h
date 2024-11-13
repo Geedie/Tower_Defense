3f542a15b14b155645106784941dbbe051d1beca
Duckkies <tvphat23@clc.fitus.edu.vn>
2024-11-13 11:17:06 +0700

    update ctower.h

--- header files/ctower.h
+++ header files/ctower.h
@@ -1,21 +1,22 @@
 #pragma once
-#include "cbullet.h"
-class ctower {
-	cpoint _location;
-	cbullet _cb;
+
+#include "Bullet.h"
+#include "Enemy.h"
+// Đối tượng tháp
+class Tower {
+private:
+    int x, y;              // Tọa độ của tháp
+    vector<Bullet> bullets; // Danh sách các viên đạn
 public:
-	ctower() { _location = { 0,0,0 }; }
-	cpoint getLocation() { return _location; }
-	void setLocation(cpoint tl) {
-		if (...) { _location = tl; }
-	}
-	void setMapForBullet(cpoint[][cpoint::MAP_SIZE]);
-	void calcPathBullet() {
-		_cb.calcPathBullet(_location);
-	}
-	// Returning references is quite dangerous. 
-	// You need to build test functions for the cbullet 
-	// class before changing the value of the internal 
-	// properties.
-	cbullet& getBullet() { return _cb; }
+    vector<Bullet> Bullets() {
+        return bullets;
+    }
+    int X() { return x; }
+    int Y() { return y; }
+    Tower(int posX, int posY) : x(posX), y(posY) {}
+
+    // Bắn đạn nếu kẻ địch trong tầm bắn
+    void shoot(Enemy& enemy);
+    // Cập nhật vị trí của đạn
+    void updateBullets();
 };
